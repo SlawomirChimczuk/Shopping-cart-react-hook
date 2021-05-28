99d77907479a6c382142226e27a709bb11a3d59d
Slawek <slawomirchim@outlook.com>
2021-05-28 13:12:19 +0200

    first commit

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ItemsList from './components/ItemsList'
+
+function HomePage() {
+    return (
+        <div>
+            1234
+            <ItemsList />
+        </div> 
+    )
+}
+
+export default HomePage
---
+++ src/pages/HomePage/components/ItemsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemsList() {
+    return (
+        <div>
+            5678
+        </div>
+    )
+}
+
+export default ItemsList 
